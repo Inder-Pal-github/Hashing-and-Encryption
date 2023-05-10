be66678f395c8c01e0c682561a8d4d287605cb9e
Inder-Pal-github <inder39811@gmail.com>
2023-05-10 15:37:55 +0530

    Hashing and Encryption/Decryption

---
+++ encryption-decyption.js
@@ -0,0 +1,35 @@
+const crypto = require("crypto");
+
+// Encryption
+     // symmetric encryption ( using same key for encryption and decryption)
+
+const algorithm = "aes-256-cbc";
+
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+function encrypt(text) {
+  let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return { iv: iv.toString("hex"), encryptedData: encrypted.toString("hex") };
+}
+
+// Decryption
+
+// same data for multiple times
+
+function decrypt(text) {
+  let iv = Buffer.from(text.iv, "hex");
+  let encryptedText = Buffer.from(text.encryptedData, "hex");
+  let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
+
+let encryptMessage = encrypt("My-secret-message")
+
+console.log("Encrypted",encryptMessage);
+
+console.log(decrypt(encryptMessage));
---
+++ hasing.js
@@ -0,0 +1,33 @@
+// Using bcrypt library
+
+// const bcrypt = require("bcrypt");
+
+// const password = "my-password-to-gmail"
+
+// const hashedPassword = bcrypt.hashSync(password,10);
+// // every hash generate is always unique => same data to same hash value
+
+// console.log("Your hashed password:  ",hashedPassword);
+
+// const passwordByUser = "my-password-to-facebook";
+
+// const checkPassword = bcrypt.compareSync(passwordByUser,hashedPassword);
+
+// console.log(checkPassword?"Password is correct":"Password is incorrect");
+
+
+
+// /// Using crypto module
+
+const crypto = require("crypto");
+
+const myPassword = "password123"
+
+const hashUsingCrypto = crypto.createHash("sha256").update(myPassword).digest("base64");
+
+// console.log("Hashed password using crypto: ",hashUsingCrypto);
+
+const userProvidedPassword = "password123"
+const checkPassword = crypto.createHash("sha256").update(userProvidedPassword).digest("base64");
+
+console.log(checkPassword===hashUsingCrypto);
